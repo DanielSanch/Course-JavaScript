e6c2a78275a8d26efc20bd1b302712ea505e6c04
Daniel Sanchez <44098522+DanielSanch@users.noreply.github.com>
2022-08-09 17:58:20 -0500

    Update

--- 2. ES6/ES6.js
+++ 2. ES6/ES6.js
@@ -16,7 +16,7 @@
 
 
 //------------------------------------------------------------------------
-// ** Muta un arreflo declarado con const **
+// ** Muta un arreglo declarado con const **
 
 // Los objetos incluyendo arreglos y funciones, asignados a una variable 
 // usando const siguen siendo mutables.
@@ -52,7 +52,7 @@ let obj = {
 
 
 //------------------------------------------------------------------------
-// ** Usa funciones fleca para escribir funciones anonimas de manera breve **
+// ** Usa funciones flecha para escribir funciones anonimas de manera breve **
 
 // Antes
 const myFunc = function() {
@@ -61,13 +61,13 @@ const myFunc = function() {
 }
 
 // Ahora 
-const myFunc = () => {
+const myFunc1 = () => {
     const myVar = "value";
     return myVar;
 }
 
 //Aun mejor
-const myFunc = () => "value";
+const myFunc2 = () => "value";
 
 // Ejemplo
 const magic = () => new Date();
@@ -96,10 +96,88 @@ multiplier(4,2);
 // ** Establece parametros por defecto para tus funciones **
 
 /*
-    ES6 indtroduce parametros por defecto para funciones
+    ES6 introduce parametros por defecto para funciones
 */
 
 const greeting = (name = "Anonymous") => "Hello " + name;
 
 console.log(greeting("John"));  // Hello John
 console.log(greeting());        // Hello Anonymous
+
+
+
+
+
+//------------------------------------------------------------------------
+// ** Utiliza el parametro rest con parametros de funcion **
+
+/**
+ * ES6 introduce el parametro rest para los parametros de funcion. Con el 
+ * parametro res, puedes crear funciones que tomen un numero variable de
+ * argumentos. Estos argumentos se almacenen en un arregloa al que se puede
+ * acceder mas tarde desde dentro de la funcion.
+ */
+
+ function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+  }
+  console.log(howMany(0, 1, 2)); //3
+  console.log(howMany("string", null, [1, 2, 3], { })); //4
+
+/**
+ * El parametro rest elimina la necesidad de comprobar el arreglo args y nos 
+ * permite aplicar map(). filter(), reduce(). 
+ */
+
+const product = (...n) => {
+    return n.reduce((a, b) => a * b, 1);
+}
+console.log(product(2, 4, 6)); //48
+
+
+
+
+
+//------------------------------------------------------------------------
+// ** Utiliza el operador de propagacion para evaluar los arreglos en el lugar **
+
+/**
+ * ES6 introduce el operador de propagacion, que nos permite expandir arreglos y 
+ * otras expresiones en lugares donde se esperan multiples parametros o elementos.
+ */
+
+const arr = [6, 89, 3, 45];
+const maximus = Math.max(...arr); // --> Arreglo desempacado (Propaga el arreglo)
+// * Solo funciona en un argumento de funcion y en un arreglo literal
+
+// Math.max espera normalemnte argumentos separados por comas, pero no un arreglo.
+// Colocando ... el operador de propagacion hace que sea mas facil leer.
+
+
+
+
+
+//------------------------------------------------------------------------
+// ** Usa sintaxis de desestructuracion para extraer valores de objetos **
+
+/**
+ * La sintaxis de desestructuracion es una sintaxis especial introducida en ES6,
+ * para asignar los valores directamente desde un objeto.
+ */
+
+// Codigo ES5
+/*
+const user = { name: 'John Doe', age: 34 };
+
+const name = user.name;
+const age = user.age;
+*/
+
+// Codigo ES6
+const user = { name: 'John Doe', age: 34 };
+
+const { name, age } = user;
+// Deben mantener el mismo nombre, lo que hace es solo desestructuracion del objeto
+
+// Para cambiar el nombre se usa el siguiente codigo
+const { name: userName, age: userAge } = user;
