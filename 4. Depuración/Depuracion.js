4a6567f8d4dda0811f64f9faa056e3389023dabf
Daniel Sanchez <44098522+DanielSanch@users.noreply.github.com>
2022-08-19 17:11:08 -0500

    Update

---
+++ 4. Depuración/Depuracion.js
@@ -0,0 +1,17 @@
+//------------------------------------------------------------------------
+// ** Usa la consola de JavaScript 
+
+// console.log()
+
+// console.clear()
+
+// typeof => Ver el tipo de dato
+/*          Boolean
+            Null
+            Undefined
+            Number
+            String
+            Symbol (ES6)
+            BigInt (ES2020)
+            Object (Arreglos = Tipo de objeto)
+*/
