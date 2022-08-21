c4da8d5e10db2d198b140f6523a4b67970792ed2
Daniel Sanchez <44098522+DanielSanch@users.noreply.github.com>
2022-08-21 16:35:27 -0500

    Update problemas

---
+++ 6. Algoritmos basicos/10. Boo who (Boolean quien).js
@@ -0,0 +1,12 @@
+/**
+ * Comprueba si el valor está clasificado como booleano primitivo. 
+ * Devuelve true o false.
+ * 
+ * Booleanos primitivos son true y false.
+ */
+
+ function booWho(bool) {
+    return typeof bool == "boolean" ? true : false; 
+  }
+  
+  console.log(booWho(true));
